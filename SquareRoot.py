4afbb0f4dab885ba96f147d2fff2de193de95464
Hitesh Bhavsar <46281517+hiteshbhavsar@users.noreply.github.com>
2021-02-19 13:23:59 -0800

    Create SquareRoot.py

---
+++ SquareRoot.py
@@ -0,0 +1,11 @@
+class Solution:
+    def mySqrt(self, x):
+        '''
+        Runtime: 28 ms, faster than 94.30% of Python3 online submissions for Sqrt(x).
+        Memory Usage: 14 MB, less than 98.21% of Python3 online submissions for Sqrt(x).
+        '''
+        """
+        :type x: int
+        :rtype: int
+        """
+        return int(math.sqrt(x))
