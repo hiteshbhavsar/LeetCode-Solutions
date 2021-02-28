99f9db513c29ccc14786960cd87146e6830a271a
Hitesh Bhavsar <46281517+hiteshbhavsar@users.noreply.github.com>
2021-02-27 17:42:23 -0800

    Create ToLowerCase.py

---
+++ ToLowerCase.py
@@ -0,0 +1,7 @@
+class Solution:
+    '''
+    Runtime: 28 ms, faster than 78.42% of Python3 online submissions for To Lower Case.
+    Memory Usage: 14.2 MB, less than 70.28% of Python3 online submissions for To Lower Case.
+'''
+    def toLowerCase(self, str: str) -> str:
+        return str.lower()
