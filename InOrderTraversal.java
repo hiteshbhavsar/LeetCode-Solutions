9e5d4b6df559121bda557deba89f5100edf7f602
Hitesh Bhavsar <46281517+hiteshbhavsar@users.noreply.github.com>
2021-04-01 15:28:31 -0700

    Create InOrderTraversal.java

---
+++ InOrderTraversal.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class InOrderTraversal {
+    /*
+    Runtime: 0 ms, faster than 100.00% of Java online submissions for Binary Tree Inorder Traversal.
+Memory Usage: 37.5 MB, less than 18.81% of Java online submissions for Binary Tree Inorder Traversal.
+    */
+    ArrayList<Integer> list=new ArrayList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if(root==null) return list;
+        
+           inOrder(root); 
+        return list;
+    }
+    
+    public void inOrder(TreeNode root)
+    {
+        if(root==null)
+        {
+            return;
+        }
+        
+        inOrder(root.left);
+        list.add(root.val);
+        inOrder(root.right);
+    }
+}
