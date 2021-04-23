0a0e53f8265a58130620ca14a8f38aaf977d0139
Hitesh Bhavsar <46281517+hiteshbhavsar@users.noreply.github.com>
2021-04-22 18:25:16 -0700

    Create OddEvenList.java

---
+++ OddEvenList.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+/*
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Odd Even Linked List.
+Memory Usage: 38.6 MB, less than 59.39% of Java online submissions for Odd Even Linked List.
+*/
+class OddEvenList {
+    public ListNode oddEvenList(ListNode head) {
+     if (head == null) return null;
+        ListNode odd = head, even = head.next, evenHead = even;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}
