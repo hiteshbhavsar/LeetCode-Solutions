6cb92967f4933929052c096c37dc21861cc002a0
Hitesh Bhavsar <46281517+hiteshbhavsar@users.noreply.github.com>
2021-05-08 18:03:34 -0700

    Create PreorderTraversal.java

---
+++ PreorderTraversal.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class PreorderTraversal {
+    /*
+    Runtime: 0 ms, faster than 100.00% of Java online submissions for Binary Tree Preorder Traversal.
+Memory Usage: 36.9 MB, less than 95.90% of Java online submissions for Binary Tree Preorder Traversal.
+    */
+    ArrayList<Integer> list=new ArrayList<>();
+    public List<Integer> preorderTraversal(TreeNode root) {
+        preOrder(root);
+        return list;
+    }
+    
+    public void preOrder(TreeNode root)
+    {
+        if(root==null)
+            return;
+        list.add(root.val);
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+}
