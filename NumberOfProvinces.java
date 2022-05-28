1dfa5e08fa2e7e5d77872fbe87d520b93c43349b
Hitesh Bhavsar <46281517+hiteshbhavsar@users.noreply.github.com>
2022-05-28 17:31:58 -0500

    Create NumberOfProvinces.java

---
+++ NumberOfProvinces.java
@@ -0,0 +1,35 @@
+public class NumberOfProvinces {
+    /*
+    Runtime: 8 ms, faster than 15.28% of Java online submissions for Number of Provinces.
+Memory Usage: 54.4 MB, less than 28.06% of Java online submissions for Number of Provinces.
+    */
+    int find(int parent[], int i) {
+        if (parent[i] == -1)
+            return i;
+        return find(parent, parent[i]);
+    }
+
+    void union(int parent[], int x, int y) {
+        int xset = find(parent, x);
+        int yset = find(parent, y);
+        if (xset != yset)
+            parent[xset] = yset;
+    }
+    public int findCircleNum(int[][] M) {
+        int[] parent = new int[M.length];
+        Arrays.fill(parent, -1);
+        for (int i = 0; i < M.length; i++) {
+            for (int j = 0; j < M.length; j++) {
+                if (M[i][j] == 1 && i != j) {
+                    union(parent, i, j);
+                }
+            }
+        }
+        int count = 0;
+        for (int i = 0; i < parent.length; i++) {
+            if (parent[i] == -1)
+                count++;
+        }
+        return count;
+    }
+}
