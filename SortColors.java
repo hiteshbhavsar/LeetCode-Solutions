2ef4111b203a3f512b460257ba9e5d8b01d3f0ac
Hitesh Bhavsar <46281517+hiteshbhavsar@users.noreply.github.com>
2022-07-12 20:44:03 -0500

    Update SortColors.java

--- SortColors.java
+++ SortColors.java
@@ -2,6 +2,42 @@ class SortColors {
     public void sortColors(int[] nums) {
         /*
         Runtime: 0 ms, faster than 100.00% of Java online submissions for Sort Colors.
+Memory Usage: 42.4 MB, less than 55.22% of Java online submissions for Sort Colors.
+        */
+       /*int zero=0,one=0,two=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==0)
+            {
+                zero++;
+            }
+            else if(nums[i]==1)
+            {
+                one++;
+            }
+            else
+            {
+                two++;
+            }
+        }
+        
+        int index=0;
+        
+        for(int i=0;i<zero;i++)
+        {
+            nums[index++]=0;
+        }
+        for(int i=0;i<one;i++)
+        {
+            nums[index++]=1;
+        }
+        for(int i=0;i<two;i++)
+        {
+            nums[index++]=2;
+        }
+        */
+        /*
+        Runtime: 0 ms, faster than 100.00% of Java online submissions for Sort Colors.
 Memory Usage: 37.4 MB, less than 76.23% of Java online submissions for Sort Colors.
         */
        /* Arrays.sort(nums);*/
@@ -11,6 +47,8 @@ Memory Usage: 37.4 MB, less than 76.23% of Java online submissions for Sort Colo
 Memory Usage: 37.1 MB, less than 96.02% of Java online submissions for Sort Colors.
         
         */
+        
+        
          if(nums.length==0||nums.length==1) return;
         int start=0;
         int end=nums.length-1;
